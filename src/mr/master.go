f5a680ea96e8809e4eef3969ae1fb3b2ef8a0ae2
Prashant Pal <prashant.pal@harness.io>
2021-07-05 11:51:57 +0530

    Map reduce Course work

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,29 +1,104 @@
 package mr
 
-import "log"
+import (
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+type TaskStatus int
+
+const (
+	IDLE      TaskStatus = iota
+	RUNNING   TaskStatus = iota
+	COMPLETED TaskStatus = iota
+)
+
+type TaskInfo struct {
+	status  TaskStatus
+	wid     int
+	StartTs int64
+	EndTs int64
+}
+
+type Tasks map[interface{}]*TaskInfo
 
 type Master struct {
-	// Your definitions here.
-
+	WStatus           map[int]string
+	MTasks            Tasks
+	MapTaskNumber     int
+	RTasks            Tasks
+	IntermediateFiles map[int][]string
+	nReducers         int
+	mu                sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (m *Master) AcquireTask(args *AcquireTaskArgs, reply *AcquireTaskReply) error {
+	m.mu.Lock()
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+	if m.WStatus[args.Wid]==""  {
+		m.WStatus[args.Wid] = "IDLE"
+	}
+
+	mapTask := m.PickMapTask()
+
+	if mapTask != nil {
+		reply.MapTask = mapTask
+		reply.Done = false
+		m.WStatus[args.Wid] = "BUSY"
+		m.mu.Unlock()
+		return nil
+	}
+
+	if !m.CheckAllMapTasksDone() {
+		reply.Done = false
+		m.WStatus[args.Wid] = "IDLE"
+		m.mu.Unlock()
+		return nil
+	}
+
+	reduceTask := m.PickReduceTask()
+	if reduceTask != nil {
+		reply.ReduceTask = reduceTask
+		reply.Done = false
+		m.WStatus[args.Wid] = "BUSY"
+		m.mu.Unlock()
+		return nil
+
+	}
+
+	m.mu.Unlock()
+	reply.Done = m.Done()
+
+	return nil
+
+}
+
+func (m *Master) MapTaskDone(args *MapTaskDoneArgs, reply *MapTaskDoneReply) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.WStatus[args.Wid] = "IDLE"
+	m.MTasks[args.InputFile].status = COMPLETED
+	m.MTasks[args.InputFile].EndTs = time.Now().Unix()
+	for i := 0; i < m.nReducers; i++ {
+		m.IntermediateFiles[i] = append(m.IntermediateFiles[i], args.IntermediateFiles[i])
+	}
 	return nil
 }
 
+func (m *Master) ReduceTaskDone(args *ReduceTaskDoneArgs, reply *ReduceTaskDoneReply) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.WStatus[args.Wid] = "IDLE"
+	m.RTasks[args.ReduceNumber].EndTs = time.Now().Unix()
+	m.RTasks[args.ReduceNumber].status = COMPLETED
+	return nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -46,12 +121,56 @@ func (m *Master) server() {
 // if the entire job has finished.
 //
 func (m *Master) Done() bool {
-	ret := false
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	for _, val := range m.RTasks {
+		if val.status == IDLE || val.status == RUNNING {
+			return false
+		}
+	}
+	return true
+}
 
-	// Your code here.
+func (m *Master) PickMapTask() *MapTask {
+	var mapTask *MapTask = nil
 
+	for key, val := range m.MTasks {
+		filePath := key.(string)
+		if val.status == IDLE {
+			mapTask = &MapTask{
+				Filepath:      filePath,
+				MapTaskNumber: m.MapTaskNumber,
+				NReducers:     m.nReducers,
+			}
+			m.MapTaskNumber++
+			m.MTasks[filePath] = &TaskInfo{status: RUNNING, StartTs: time.Now().Unix()}
+			break
+		}
+	}
+	return mapTask
+}
 
-	return ret
+func (m *Master) CheckAllMapTasksDone() bool {
+	for _, val := range m.MTasks {
+		if val.status == IDLE || val.status == RUNNING {
+			return false
+		}
+	}
+	return true
+}
+
+func (m *Master) PickReduceTask() *ReduceTask {
+	var reduceTask *ReduceTask = nil
+	for k, val := range m.RTasks{
+		if val.status == IDLE {
+			reduceTask = &ReduceTask{
+				IntermediateFiles: m.IntermediateFiles[k.(int)],
+				ReduceNumber:      k.(int),
+			}
+			break
+		}
+	}
+	return reduceTask
 }
 
 //
@@ -60,10 +179,19 @@ func (m *Master) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
+	m := Master{nReducers: nReduce, MTasks: Tasks{}, RTasks: Tasks{}, WStatus: make(map[int]string), IntermediateFiles: make(map[int][]string)}
 
+	for idx, file := range files {
+		if idx == 0 {
+			m.MapTaskNumber = idx
+		}
+		m.MTasks[file] = &TaskInfo{status: IDLE, StartTs: -1, EndTs: -1}
+	}
+
+	for i := 0; i < nReduce; i++ {
+		m.RTasks[i] = &TaskInfo{status: IDLE, StartTs: -1, EndTs: -1}
+	}
 	// Your code here.
-
 
 	m.server()
 	return &m
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,15 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
-
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,6 +18,14 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce
@@ -24,18 +37,137 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
 
+	for {
+		reply := AcquireTask()
+
+		if reply.Done {
+			log.Printf("Got a Done signal from Master Exiting")
+			break
+		}
+
+		if reply.MapTask != nil {
+			performMapTask(reply.MapTask, mapf)
+		}
+
+		if reply.ReduceTask != nil {
+			performReduceTask(reply.ReduceTask, reducef)
+		}
+	}
 	// Your worker implementation here.
 
-	// uncomment to send the Example RPC to the master.
-	// CallExample()
+}
 
+func performReduceTask(task *ReduceTask, reducef func(string, []string) string) {
+	intermediate := collectKV(task.IntermediateFiles)
+	sort.Sort(ByKey(intermediate))
+	ofileName := fmt.Sprintf("mr-out-%v", task.ReduceNumber)
+	tempFile, err := ioutil.TempFile(".", ofileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", ofileName)
+	}
+
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		var values []string
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	_ = os.Rename(tempFile.Name(), ofileName)
+	RespondToReduceTask(task.ReduceNumber)
+}
+
+func RespondToReduceTask(reduceNumber int) {
+	args := ReduceTaskDoneArgs{
+		ReduceNumber: reduceNumber,
+		Wid:          os.Getpid(),
+	}
+
+	reply := ReduceTaskDoneReply{}
+	call("Master.ReduceTaskDone", &args, &reply)
+}
+
+func collectKV(files []string) []KeyValue {
+	var intermediate []KeyValue
+
+	for _, f := range files {
+		dat, err := ioutil.ReadFile(f)
+		if err != nil {
+			fmt.Printf("FileName %v\n", f)
+			fmt.Println("Read error: ", f, err.Error())
+		}
+		var input []KeyValue
+		err = json.Unmarshal(dat, &input)
+		if err != nil {
+			fmt.Println("Unmarshal error: ", err.Error())
+		}
+
+		intermediate = append(intermediate, input...)
+	}
+	return intermediate
+}
+
+func performMapTask(task *MapTask, mapf func(string, string) []KeyValue) {
+	kva := applyMap(task, mapf)
+	sort.Sort(ByKey(kva))
+
+	partitions := make([][]KeyValue, task.NReducers)
+
+	for _, val := range kva {
+		partition := ihash(val.Key) % task.NReducers
+		partitions[partition] = append(partitions[partition], val)
+	}
+
+	intermediateFiles := make([]string, task.NReducers)
+	for i := 0; i < task.NReducers; i++ {
+		interMediateFile := fmt.Sprintf("mr-%d-%d", task.MapTaskNumber, i)
+		ofile, _ := os.Create(interMediateFile)
+		b, err := json.Marshal(partitions[i])
+		if err != nil {
+			fmt.Println("Marshal error: ", err)
+		}
+		ofile.Write(b)
+		ofile.Close()
+		intermediateFiles[i] = interMediateFile
+	}
+	RespondToMapTask(intermediateFiles, task.Filepath)
+}
+
+func applyMap(task *MapTask, mapf func(string, string) []KeyValue) []KeyValue {
+	file, err := os.Open(task.Filepath)
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("cannot open %v", task.Filepath)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", task.Filepath)
+	}
+	return mapf(task.Filepath, string(content))
+}
+
+func RespondToMapTask(intermediateFiles []string, inputFile string) {
+	args := MapTaskDoneArgs{
+		IntermediateFiles: intermediateFiles,
+		InputFile:         inputFile,
+		Wid:               os.Getpid(),
+	}
+
+	reply := MapTaskDoneReply{}
+	call("Master.MapTaskDone", &args, &reply)
 }
 
 //
@@ -43,22 +175,18 @@ func Worker(mapf func(string, string) []KeyValue,
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func CallExample() {
+func AcquireTask() AcquireTaskReply {
 
 	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
+	args := AcquireTaskArgs{Wid: os.Getpid()}
 
 	// declare a reply structure.
-	reply := ExampleReply{}
+	reply := AcquireTaskReply{}
 
 	// send the RPC request, wait for the reply.
-	call("Master.Example", &args, &reply)
+	call("Master.AcquireTask", &args, &reply)
+	return reply
 
-	// reply.Y should be 100.
-	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
 //
